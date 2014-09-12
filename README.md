56ee01a974c313fce19398caf9a873b04e08106e
Keith Wood <kbwood@iinet.net.au>
2014-09-13 08:59:58 +1000

    Release 1.1.1

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 jQuery Signature
 ================
 
-This plugin sets a div to allow capture of a signature and converts it to JSON for storage. Requires jQuery UI and ExCanvas (for IE).
+This plugin sets a div to allow capture of a signature and converts it to JSON or SVG for storage. Requires jQuery UI and ExCanvas (for IE).
 
 Have a look at the plugin Web site, http://keith-wood.name/signature.html, for demonstrations of its abilities.
 
--- jquery.signature.js
+++ jquery.signature.js
@@ -1,8 +1,8 @@
 /* http://keith-wood.name/signature.html
-   Signature plugin for jQuery UI v1.1.0.
+   Signature plugin for jQuery UI v1.1.1.
    Requires excanvas.js in IE.
    Written by Keith Wood (kbwood{at}iinet.com.au) April 2012.
-   Available under the MIT (https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt) license. 
+   Available under the MIT (https://github.com/jquery/jquery/blob/master/LICENSE.txt) license. 
    Please attribute the author if you use it. */
 
 (function($) { // Hide scope, no $ conflict
@@ -43,7 +43,7 @@ var signatureOverrides = {
 			if (G_vmlCanvasManager) { // Requires excanvas.js
 				G_vmlCanvasManager.initElement(this.canvas);
 			}
-		this.ctx = this.canvas.getContext('2d');
+			this.ctx = this.canvas.getContext('2d');
 		}
 		this._refresh(true);
 		this._mouseInit();
@@ -157,20 +157,29 @@ var signatureOverrides = {
 	/* Convert the captured lines to JSON text.
 	   @return  (string) the JSON text version of the lines */
 	toJSON: function() {
-		var json = '{"lines":[';
-		var lineSep = '';
-		$.each(this.lines, function() {
-			json += lineSep + '[';
-			var pointSep = '';
-			$.each(this, function() {
-				json += pointSep + '[' + this[0] + ',' + this[1] + ']';
-				pointSep = ',';
-			});
-			json += ']';
-			lineSep = ',';
-		});
-		json += ']}';
-		return json;
+		return '{"lines":[' + $.map(this.lines, function(line) {
+			return '[' + $.map(line, function(point) {
+					return '[' + point + ']';
+				}) + ']';
+		}) + ']}';
+	},
+
+	/* Convert the captured lines to SVG text.
+	   @return  (string) the SVG text version of the lines */
+	toSVG: function() {
+		return '<?xml version="1.0"?>\n<!DOCTYPE svg PUBLIC ' +
+			'"-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n' +
+			'<svg xmlns="http://www.w3.org/2000/svg" width="15cm" height="15cm">\n' +
+			'	<g fill="' + this.options.background + '">\n' +
+			'		<rect x="0" y="0" width="' + this.canvas.width +
+			'" height="' + this.canvas.height + '"/>\n' +
+			'		<g fill="none" stroke="' + this.options.color + '" stroke-width="' +
+			this.options.thickness + '">\n'+
+			$.map(this.lines, function(line) {
+				return '			<polyline points="' +
+					$.map(line, function(point) { return point + ''; }).join(' ') + '"/>\n';
+			}).join('') +
+			'		</g>\n	</g>\n</svg>\n';
 	},
 
 	/* Draw a signature from its JSON description.
@@ -179,7 +188,7 @@ var signatureOverrides = {
 	                    (string) text version of the JSON */
 	draw: function(sigJSON) {
 		this.clear(true);
-		if (typeof sigJSON == 'string') {
+		if (typeof sigJSON === 'string') {
 			sigJSON = $.parseJSON(sigJSON);
 		}
 		this.lines = sigJSON.lines || [];
@@ -187,7 +196,7 @@ var signatureOverrides = {
 		$.each(this.lines, function() {
 			ctx.beginPath();
 			$.each(this, function(i) {
-				ctx[i == 0 ? 'moveTo' : 'lineTo'](this[0], this[1]);
+				ctx[i === 0 ? 'moveTo' : 'lineTo'](this[0], this[1]);
 			});
 			ctx.stroke();
 		});
@@ -197,7 +206,7 @@ var signatureOverrides = {
 	/* Determine whether or not any drawing has occurred.
 	   @return  (boolean) true if not signed, false if signed */
 	isEmpty: function() {
-		return this.lines.length == 0;
+		return this.lines.length === 0;
 	},
 
 	/* Remove the signature functionality. */
